6394d1b845296361d462803fd80cde119351f734
Bogdan Angelov <bgd.angelov@gmail.com>
2020-10-15 19:00:58 +0300

    Update problem from Arrays

--- 01.Fundamentals with C#/3. Arrays/23. Longest Increasing Sequence/Program.cs
+++ 01.Fundamentals with C#/3. Arrays/23. Longest Increasing Sequence/Program.cs
@@ -7,29 +7,47 @@ namespace _23._Longest_Increasing_Sequence
     {
         static void Main(string[] args)
         {
-            int[] array = Console.ReadLine().Split()
-                .Select(int.Parse)
-                .ToArray();
-            int length = 0;
-            int digitHolder = 0;
-            int[] finalArray = new int[] { };
+            int[] nums = Console.ReadLine()
+                               .Split()
+                               .Select(int.Parse)
+                               .ToArray();
+            int maxLength = 0;
+            int lastIndex = -1;
+            int[] len = new int[nums.Length];
+            int[] prev = new int[nums.Length];
 
-            for (int i = 0; i < array.Length; i++)
+            for (int i = 0; i < nums.Length; i++)
             {
-                if (array.Length <= 1)
-                {
-                    Console.WriteLine($"{array[i]}");
-                    return;
-                }
-                else
-                {
-                    if (digitHolder )
-                    {
+                len[i] = 1;
+                prev[i] = -1;
 
+                for (int k = 0; k < i; k++)
+                {
+                    if (nums[k] < nums[i] && len[k] + 1 > len[i])
+                    {
+                        len[i] = len[k] + 1;
+                        prev[i] = k;
                     }
-                    digitHolder = array[i];
+                }
+
+                if (len[i] > maxLength)
+                {
+                    maxLength = len[i];
+                    lastIndex = i;
                 }
             }
+
+            int[] LIS = new int[maxLength];
+            int currentIndex = maxLength - 1;
+
+            while (lastIndex != -1)
+            {
+                LIS[currentIndex] = nums[lastIndex];
+                currentIndex--;
+                lastIndex = prev[lastIndex];
+            }
+
+            Console.WriteLine(string.Join(" ", LIS));
         }
     }
 }
