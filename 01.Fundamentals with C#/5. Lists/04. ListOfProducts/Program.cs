4f7113310f5ec1c98fd2ab8416ecd54723226200
Bogdan Angelov <bgd.angelov@gmail.com>
2020-10-15 18:41:01 +0300

    Add Lists from Fundamentals

---
+++ 01.Fundamentals with C#/5. Lists/04. ListOfProducts/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._ListOfProducts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfProducts = int.Parse(Console.ReadLine());
+
+            List<string> products = new List<string>();
+
+            for (int i = 0; i < numberOfProducts; i++)
+            {
+                products.Add(Console.ReadLine());
+            }
+
+            products.Sort();
+
+            int index = 1;
+
+            foreach (string item in products)
+            {
+                Console.WriteLine($"{index}.{item}");
+                index++;
+            }
+        }
+    }
+}
---
+++ 01.Fundamentals with C#/5. Lists/07. ListManipulationAdvanced/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _07._ListManipulationAdvanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> originalList = Console.ReadLine()
+               .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToList();
+
+            List<int> manipulateList = new List<int>();
+            manipulateList.AddRange(originalList);
+
+            ReturnManipulation(originalList, manipulateList);
+        }
+
+        static void ReturnManipulation(List<int> originalList, List<int> manipulateList)
+        {
+            string command = string.Empty;
+
+            bool ischanged = false;
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] commandArr = command.Split();
+                
+                switch (commandArr[0])
+                {
+                    case "Add":
+                        originalList.Add(int.Parse(commandArr[1]));
+                        ischanged = true;
+                        break;
+                    case "Remove":
+                        originalList.RemoveAll(x => x == int.Parse(commandArr[1]));
+                        ischanged = true;
+                        break;
+                    case "RemoveAt":
+                        originalList.RemoveAt(int.Parse(commandArr[1]));
+                        ischanged = true;
+                        break;
+                    case "Insert":
+                        originalList.Insert(int.Parse(commandArr[2]), int.Parse(commandArr[1]));
+                        ischanged = true;
+                        break;
+                    case "Contains":
+                        if (manipulateList.Contains(int.Parse(commandArr[1])))
+                        {
+                            Console.WriteLine("Yes");
+                        }
+                        else
+                        {
+                            Console.WriteLine("No such number");
+                        }
+                        break;
+                    case "PrintEven":
+                        Console.WriteLine(string.Join(" ", manipulateList.Where(x => x % 2 == 0)));
+                        break;
+                    case "PrintOdd":
+                        Console.WriteLine(string.Join(" ", manipulateList.Where(x => x % 2 == 1)));
+                        break;
+                    case "GetSum":
+                        Console.WriteLine(string.Join(" ", manipulateList.Sum()));
+                        break;
+                    case "Filter":
+                        PrintFiltered(manipulateList, commandArr[1], int.Parse(commandArr[2]));
+                        break;
+                }
+            }
+            if (ischanged)
+            {
+                Console.WriteLine(string.Join(" ", originalList));
+            }
+        }
+        static void PrintFiltered (List<int> manipulateList, string operat, int number)
+        {
+            switch (operat)
+            {
+                case ">":
+                    Console.WriteLine(string.Join(" ", manipulateList.Where(x => x > number)));
+                    break;
+                case "<":
+                    Console.WriteLine(string.Join(" ", manipulateList.Where(x => x < number)));
+                    break;
+                case "<=":
+                    Console.WriteLine(string.Join(" ", manipulateList.Where(x => x <= number)));
+                    break;
+                default:
+                    Console.WriteLine(string.Join(" ", manipulateList.Where(x => x >= number)));
+                    break;
+            }
+        }
+    }
+}
