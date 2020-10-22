3b03f50981b2918412690d86cdf477e4dfa02080
Bogdan Angelov <bgd.angelov@gmail.com>
2020-10-22 17:21:08 +0300

    Add problems from Objects And Classes

---
+++ 01.Fundamentals with C#/6. Objects And Classes/06. StoreBoxes/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06._StoreBoxes
+{
+    class Item
+    {
+        public Item(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
