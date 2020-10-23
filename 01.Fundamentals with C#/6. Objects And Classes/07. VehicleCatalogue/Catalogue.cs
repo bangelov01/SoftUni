7d7bd6b9d1935e40a8615bc2a161b8f8d83178db
Bogdan Angelov <bgd.angelov@gmail.com>
2020-10-23 14:33:34 +0300

    Add problem to Objects And Classes

---
+++ 01.Fundamentals with C#/6. Objects And Classes/07. VehicleCatalogue/Catalogue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07._VehicleCatalogue
+{
+    class Catalogue
+    {
+        public Catalogue()
+        {
+            Cars = new List<Cars>();
+
+            Trucks = new List<Trucks>();
+        }
+        public List<Cars> Cars { get; set; }
+        public List<Trucks> Trucks { get; set; }
+    }
+}
