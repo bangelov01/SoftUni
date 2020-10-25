e541184c3479119fe521d9b56d1eff5fbc18591e
Bogdan Angelov <bgd.angelov@gmail.com>
2020-10-25 14:54:13 +0200

    Add problems to Objects And Classes

---
+++ 01.Fundamentals with C#/6. Objects And Classes/08. AdvertisementMessage/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _08._AdvertisementMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfMessages = int.Parse(Console.ReadLine());
+
+            string[] phrases = { "Excellent product.",
+                "Such a great product.",
+                "I always use that product.",
+                "Best product of its category.",
+                "Exceptional product.",
+                "I can’t live without this product." };
+
+            string[] events = { "Now I feel good.",
+                "I have succeeded with this product.",
+                "Makes miracles. I am happy of the results!",
+                "I cannot believe but now I feel awesome.",
+                "Try it yourself, I am very satisfied.",
+                "I feel great!" };
+
+            string[] authors = { "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva" };
+
+            string[] cities = { "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse" };
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < numberOfMessages; i++)
+            {
+                int phrase = rnd.Next(0, phrases.Length);
+                int eve = rnd.Next(0, events.Length);
+                int author = rnd.Next(0, authors.Length);
+                int city = rnd.Next(0, cities.Length);
+
+                Console.WriteLine($"{phrases[phrase]} {events[eve]} {authors[author]} – {cities[city]}");
+            }
+        }
+    }
+}
---
+++ 01.Fundamentals with C#/6. Objects And Classes/12. TeamworkProjects/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace _12._TeamworkProjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfTeams = int.Parse(Console.ReadLine());
+
+            List<Team> teams = new List<Team>();
+
+            for (int i = 0; i < numberOfTeams; i++)
+            {
+                string[] createTeam = Console.ReadLine().Split("-");
+
+                string creator = createTeam[0];
+                string teamName = createTeam[1];
+
+                if ((!teams.Exists(x => x.TeamName == teamName)) && (!teams.Exists(x => x.Creator == creator)))
+                {
+                    Team newTeam = new Team(creator, teamName);
+                    teams.Add(newTeam);
+                    Console.WriteLine($"Team {teamName} has been created by {creator}!");
+                }
+                else if (teams.Exists(x => x.TeamName == teamName))
+                {
+                    Console.WriteLine($"Team {teamName} was already created!");
+
+                }
+                else if (teams.Exists(x => x.Creator == creator))
+                {
+                    Console.WriteLine($"{creator} cannot create another team!");
+                }
+            }
+
+            string joinRequest = string.Empty;
+
+            while ((joinRequest = Console.ReadLine()) != "end of assignment")
+            {
+                string[] joinRequestArr = joinRequest.Split("->");
+
+                string user = joinRequestArr[0];
+                string team = joinRequestArr[1];
+
+                if (teams.Exists(x => x.TeamName == team) && (!teams.Exists(x => x.Members.Contains(user))) && (!teams.Exists(x => x.Creator.Contains(user))))
+                {
+                   int index = teams.FindIndex(x => x.TeamName == team);
+                   teams[index].Members.Add(user);
+                }
+                else if (!teams.Exists(x => x.TeamName == team))
+                {
+                    Console.WriteLine($"Team {team} does not exist!");
+                }
+                else if (teams.Exists(x => x.Members.Contains(user)) || teams.Exists(x => x.Creator.Contains(user)))
+                {
+                    Console.WriteLine($"Member {user} cannot join team {team}!");
+                }
+            }
+
+            Team[] disbanded = teams.OrderBy(x => x.TeamName)
+                                    .Where(x => x.Members.Count == 0)
+                                    .ToArray();
+
+            Team[] fullTeams = teams.OrderByDescending(x => x.Members.Count)
+                                    .ThenBy(x => x.TeamName)
+                                    .Where(x => x.Members.Count > 0)
+                                    .ToArray();
+
+            foreach (Team team in fullTeams)
+            {
+                Console.WriteLine(team);
+            }
+            Console.WriteLine("Teams to disband:");
+
+            foreach (Team team in disbanded)
+            {
+                Console.WriteLine(team.TeamName);
+            }
+        }
+    }
+}
