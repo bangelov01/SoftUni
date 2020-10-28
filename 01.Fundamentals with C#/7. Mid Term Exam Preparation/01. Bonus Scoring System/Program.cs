ef56545af8542b3b4816ecf029bc2a87be321937
Bogdan Angelov <bgd.angelov@gmail.com>
2020-10-28 17:29:35 +0200

    Add Exam Prep

---
+++ 01.Fundamentals with C#/7. Mid Term Exam Preparation/01. Bonus Scoring System/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Bonus_Scoring_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int studentCount = int.Parse(Console.ReadLine());
+            int lecturesCount = int.Parse(Console.ReadLine());
+            double bonus = double.Parse(Console.ReadLine());
+
+            double saveTotalBonus = 0;
+            double saveAtt = 0;
+
+            for (int i = 0; i < studentCount; i++)
+            {
+                double attendanceCount = double.Parse(Console.ReadLine());
+                double totalBonus = attendanceCount / lecturesCount * (5 + bonus);
+                if (totalBonus > saveTotalBonus)
+                {
+                    saveTotalBonus = totalBonus;
+                    saveAtt = attendanceCount;
+                }
+            }
+
+            Console.WriteLine($"Max Bonus: {Math.Ceiling(saveTotalBonus)}.");
+            Console.WriteLine($"The student has attended {Math.Ceiling(saveAtt)} lectures.");
+        }
+    }
+}
---
+++ 01.Fundamentals with C#/7. Mid Term Exam Preparation/02. MuOnline/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._MuOnline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine().Split("|", StringSplitOptions.RemoveEmptyEntries)
+                                                   .ToList();
+            int initialHealth = 100;
+            int bitcoins = 0;
+            int roomCount = 0;
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                string[] room = input[i].Split(" ");
+
+                string command = room[0];
+                int number = int.Parse(room[1]);
+                roomCount++;
+
+                if (command == "chest")
+                {
+                    bitcoins += number;
+                    Console.WriteLine($"You found {number} bitcoins.");
+                    continue;
+                }
+                else if (command == "potion")
+                {
+                    
+                    if ((initialHealth + number) >= 100)
+                    {
+                        int remains = 100 - initialHealth;
+                        Console.WriteLine($"You healed for {remains} hp.");
+                        initialHealth = 100;
+                    }
+                    else
+                    {
+                        initialHealth += number;
+                        Console.WriteLine($"You healed for {number} hp.");
+                    }
+                    Console.WriteLine($"Current health: {initialHealth} hp.");
+                }
+                else
+                {
+                    initialHealth -= number;
+                    if (initialHealth <= 0)
+                    {
+                        Console.WriteLine($"You died! Killed by {command}.");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You slayed {command}.");
+                        continue;
+                    }
+                }
+            }
+            if (initialHealth > 0)
+            {
+                Console.WriteLine($"You've made it!\r\nBitcoins: {bitcoins}\r\nHealth: {initialHealth}");
+            }
+            else
+            {
+                Console.WriteLine($"Best room: {roomCount}");
+            }
+        }
+    }
+}
