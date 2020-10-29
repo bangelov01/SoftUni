cdefcce1c22d3a0c2614f1966097787b1560909e
Bogdan Angelov <bgd.angelov@gmail.com>
2020-10-29 17:01:02 +0200

    Add problems to Mid Exam Prep

---
+++ 01.Fundamentals with C#/7. Mid Term Exam Preparation/04. NationalCourt/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _04._NationalCourt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int empOneCount = int.Parse(Console.ReadLine());
+            int empTwoCount = int.Parse(Console.ReadLine());
+            int empThreeCount = int.Parse(Console.ReadLine());
+            int peopleCount = int.Parse(Console.ReadLine());
+
+            int totalToAnswer = empOneCount + empTwoCount + empThreeCount;
+            int answeredForHour = 0;
+            int hours = 0;
+
+            if (peopleCount > 0)
+            {
+                while (true)
+                {
+                    if (hours % 4 == 0 && hours > 0)
+                    {
+                        hours++;
+                        continue;
+                    }
+                    else
+                    {
+                        answeredForHour += totalToAnswer;
+                        hours++;
+                    }
+                    if (answeredForHour >= peopleCount)
+                    {
+                        if (hours % 4 == 0)
+                        {
+                            hours++;
+                            break;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"Time needed: {hours}h.");
+        }
+    }
+}
