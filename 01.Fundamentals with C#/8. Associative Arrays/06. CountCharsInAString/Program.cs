26d7354c1b935f2a5be2bfd7be80d95dd0aafbd4
Bogdan Angelov <bgd.angelov@gmail.com>
2020-11-14 15:42:13 +0200

    Add to Associative arrays

---
+++ 01.Fundamentals with C#/8. Associative Arrays/06. CountCharsInAString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._CountCharsInAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] chars = Console.ReadLine().ToArray();
+
+            Dictionary<char, int> charDict = new Dictionary<char, int>();
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] != ' ')
+                {
+                    if (!charDict.ContainsKey(chars[i]))
+                    {
+                        charDict.Add(chars[i], 0);
+                    }
+                    charDict[chars[i]]++;
+                }
+            }
+
+            foreach (var item in charDict)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
