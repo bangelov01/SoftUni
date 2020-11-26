bdbaec288c5d67257c6d859fcc0254cf688b5460
Bogdan Angelov <bgd.angelov@gmail.com>
2020-11-26 17:50:49 +0200

    Add to Regular expressions

---
+++ 01.Fundamentals with C#/10. Regular Expressions/01. MatchFullName/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01._MatchFullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string regEx = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+
+            string input = Console.ReadLine();
+
+            MatchCollection output = Regex.Matches(input, regEx);
+
+            Console.WriteLine(string.Join(" ", output));
+        }
+    }
+}
---
+++ 01.Fundamentals with C#/10. Regular Expressions/03. MatchDates/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._MatchDates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @"\b(?<day>[0-2][0-9]|3[0-1])(.|-|\/)(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b";
+
+            string input = Console.ReadLine();
+
+            MatchCollection output = Regex.Matches(input, regex);
+
+            foreach (Match date in output)
+            {
+                string day = date.Groups["day"].Value;
+                string month = date.Groups["month"].Value;
+                string year = date.Groups["year"].Value;
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
