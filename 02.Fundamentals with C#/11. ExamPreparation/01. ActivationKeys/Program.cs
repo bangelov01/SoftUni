b257a02e863e64a81267597fcb7f5a544dc34ee6
Bogdan Angelov <bgd.angelov@gmail.com>
2020-12-01 22:55:05 +0200

    Add to Fundamentals exam prep

---
+++ 02.Fundamentals with C#/11. ExamPreparation/01. ActivationKeys/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace _01._ActivationKeys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string activationKey = Console.ReadLine();
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "Generate")
+            {
+                string[] commandArr = command.Split(">>>");
+
+                string action = commandArr[0];
+
+                if (action == "Contains")
+                {
+                    string substring = commandArr[1];
+
+                    if (activationKey.Contains(substring))
+                    {
+                        Console.WriteLine($"{activationKey} contains {substring}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Substring not found!");
+                    }
+                }
+                else if (action == "Flip")
+                {
+                    string upOrLow = commandArr[1];
+                    int startIndex = int.Parse(commandArr[2]);
+                    int endIndex = int.Parse(commandArr[3]);
+                    int count = endIndex - startIndex;
+                    string sub = activationKey.Substring(startIndex, count);
+
+                    if (upOrLow == "Lower")
+                    {
+                        sub = sub.ToLower();
+                    }
+                    else
+                    {
+                        sub = sub.ToUpper();
+                    }
+
+                    activationKey = activationKey.Remove(startIndex, count).Insert(startIndex, sub);
+
+                    Console.WriteLine($"{activationKey}");
+                }               
+                else if (action == "Slice")
+                {
+                    int startInd = int.Parse(commandArr[1]);
+                    int endIndex = int.Parse(commandArr[2]);
+                    int count = endIndex - startInd;
+                    activationKey = activationKey.Remove(startInd, count);
+                    Console.WriteLine($"{activationKey}");
+                }
+            }
+
+            Console.WriteLine($"Your activation key is: {activationKey}");
+        }
+    }
+}
---
+++ 02.Fundamentals with C#/11. ExamPreparation/02. EmojiDetector/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace _02._EmojiDetector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string emojiPattern = @"([:]{2}|[*]{2})([A-Z][a-z]{2,})\1";
+
+            string digitPattern = @"[\d]";
+
+            List<string> coolEmojis = new List<string>();
+
+            MatchCollection emojis = Regex.Matches(input, emojiPattern);
+            MatchCollection digits = Regex.Matches(input, digitPattern);
+
+
+            BigInteger coolFactor = new BigInteger();
+            coolFactor = 1;
+
+            foreach (Match digit in digits)
+            {
+                coolFactor *= int.Parse(digit.Value);
+            }
+
+            foreach (Match emoji in emojis)
+            {
+               int coolness = emoji.Value.Where(x => char.IsLetter(x)).Sum(x => x);
+
+                if (coolness >= coolFactor)
+                {
+                    coolEmojis.Add(emoji.Value);
+                }
+            }
+
+            Console.WriteLine($"Cool threshold: {coolFactor}");
+            Console.WriteLine($"{emojis.Count} emojis found in the text. The cool ones are:");
+
+            if (coolEmojis.Count != 0)
+            {
+                foreach (var item in coolEmojis)
+                {
+                    Console.WriteLine($"{item}");
+                }
+            }
+        }
+    }
+}
