d94da8051935aa3410c219ac2eeae66b758dba1d
Bogdan Angelov <bgd.angelov@gmail.com>
2020-12-05 19:32:53 +0200

    Add to Exam prep

---
+++ 02.Fundamentals with C#/11. Final Exam Preparation/07. SecretChat/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._SecretChat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+
+            string instructions = string.Empty;
+
+            while ((instructions = Console.ReadLine()) != "Reveal")
+            {
+                string[] instrArr = instructions.Split(":|:", StringSplitOptions.RemoveEmptyEntries);
+
+                string command = instrArr[0];
+
+                if (command == "InsertSpace")
+                {
+                    int index = int.Parse(instrArr[1]);
+                    message = message.Insert(index, " ");
+                    Console.WriteLine($"{message}");
+                }
+                else if (command == "Reverse")
+                {
+                    string sub = instrArr[1];
+
+                    if (message.Contains(sub))
+                    {
+                        int index = message.IndexOf(sub);
+                        message = message.Remove(index, sub.Length);
+                        for (int i = sub.Length - 1; i >= 0; i--)
+                        {
+                            message += sub[i];
+                        }
+                        Console.WriteLine($"{message}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+                    }
+                }
+                else
+                {
+                    string sub = instrArr[1];
+                    string repl = instrArr[2];
+
+                    message = message.Replace(sub, repl);
+                    Console.WriteLine($"{message}");
+                }
+            }
+
+            Console.WriteLine($"You have a new text message: {message}");
+        }
+    }
+}
---
+++ 02.Fundamentals with C#/11. Final Exam Preparation/08. MirrorWords/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _08._MirrorWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"([@]{1}|[#]{1})(?<firstWord>[A-Za-z]{3,})\1\1(?<secondWord>[A-Za-z]{3,})\1";
+
+            List<string> mirroredWords = new List<string>();
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            if (matches.Count == 0)
+            {
+                Console.WriteLine("No word pairs found!\r\nNo mirror words!");
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine($"{matches.Count} word pairs found!");
+
+            foreach (Match item in matches)
+            {
+                string wordOne = item.Groups["firstWord"].Value;
+                string wordTwo = item.Groups["secondWord"].Value;
+
+                StringBuilder reverseWord = new StringBuilder();
+
+
+                for (int i = wordTwo.Length - 1; i >= 0; i--)
+                {
+                    reverseWord.Append(wordTwo[i]);
+                }
+
+                if (wordOne == reverseWord.ToString())
+                {
+                    string toAdd = $"{wordOne} <=> {wordTwo}";
+                    mirroredWords.Add(toAdd);
+                }
+            }
+
+            if (mirroredWords.Count > 0)
+            {
+                Console.WriteLine("The mirror words are:");
+                Console.WriteLine(string.Join(", ", mirroredWords));
+            }
+            else
+            {
+                Console.WriteLine("No mirror words!");
+            }
+        }
+    }
+}
