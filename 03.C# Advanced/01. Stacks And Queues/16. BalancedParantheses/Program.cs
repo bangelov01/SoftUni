e4a64092bf970632d9c221cd565e47983609a6c6
Bogdan Angelov <bgd.angelov@gmail.com>
2021-01-17 09:37:19 +0200

    Add to StacksAndQueues

---
+++ 03.C# Advanced/01. Stacks And Queues/16. BalancedParantheses/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _16._BalancedParantheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string parantheses = Console.ReadLine();
+            List<char> paraList = new List<char>(parantheses);
+
+            Stack<char> leftPara = new Stack<char>();
+
+            for (int i = 0; i < parantheses.Length; i++)
+            {
+                if (parantheses[i] == '(' || parantheses[i] == '{' || parantheses[i] == '[')
+                {
+                    leftPara.Push(parantheses[i]);
+                    paraList.Remove(parantheses[i]);
+                }
+                else
+                {
+                    if (leftPara.Count > 0)
+                    {
+                        char currPara = leftPara.Peek();
+                        if ((currPara == '(' && parantheses[i] == ')') ||
+                            (currPara == '[' && parantheses[i] == ']') ||
+                            (currPara == '{' && parantheses[i] == '}'))
+                        {
+                            leftPara.Pop();
+                            paraList.Remove(parantheses[i]);
+                        }
+                    }
+                }
+            }
+            if (paraList.Count == 0 && leftPara.Count == 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
---
+++ 03.C# Advanced/01. Stacks And Queues/17. SimpleTextEditor/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _17._SimpleTextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int operationsNumber = int.Parse(Console.ReadLine());
+
+            StringBuilder text = new StringBuilder();
+
+            Stack<string> state = new Stack<string>();
+
+            state.Push(text.ToString());
+
+            for (int i = 0; i < operationsNumber; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                int command = int.Parse(input[0]);
+
+                if (command == 1)
+                {
+                    string toAopend = input[1];
+                    text.Append(toAopend);
+                    state.Push(text.ToString());
+                }
+                else if (command == 2)
+                {
+                    int countToErase = int.Parse(input[1]);
+                    text.Remove(text.Length - countToErase, countToErase);
+                    state.Push(text.ToString());
+                }
+                else if (command == 3)
+                {
+                    int elementIndex = int.Parse(input[1]);
+                    Console.WriteLine(text[elementIndex - 1]);
+                }
+                else
+                {
+                    state.Pop();
+                    text.Clear();
+                    text.Append(state.Peek());
+                }
+            }
+        }
+    }
+}
