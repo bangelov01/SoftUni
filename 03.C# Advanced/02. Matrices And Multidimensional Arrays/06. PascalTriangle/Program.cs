7d95a67bd3da0904dabeb230cc15439b3ed71777
Bogdan Angelov <bgd.angelov@gmail.com>
2021-01-18 15:59:28 +0200

    Add to Matrices

---
+++ 03.C# Advanced/02. Matrices And Multidimensional Arrays/06. PascalTriangle/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _06._PascalTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+
+            long[][] pascalTriangle = new long[n][];
+            pascalTriangle[0] = new long[1] { 1 };
+
+            for (int row = 1; row < pascalTriangle.Length; row++)
+            {
+                pascalTriangle[row] = new long[row + 1];
+
+                for (int col = 0; col < pascalTriangle[row].Length; col++)
+                {
+                    if (col == 0)
+                    {
+                        pascalTriangle[row][col] = 1;
+                        continue;
+                    }
+                    else if (col == pascalTriangle[row].Length - 1)
+                    {
+                        pascalTriangle[row][col] = 1;
+                        break;
+                    }
+                    else
+                    {
+                        pascalTriangle[row][col] = pascalTriangle[row - 1][col - 1] + 
+                        pascalTriangle[row - 1][col];
+                    }
+                }
+            }
+
+            for (int i = 0; i < pascalTriangle.Length; i++)
+            {
+                for (int j = 0; j < pascalTriangle[i].Length; j++)
+                {
+                    Console.Write(pascalTriangle[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 03.C# Advanced/02. Matrices And Multidimensional Arrays/07. DiagonalDifference/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._DiagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[size, size];
+
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    matrix[rows, cols] = nums[cols];
+                }
+            }
+
+
+            int primaryDiagonalSum = 0;
+            int secondaryDiagonalSum = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int neededIndex = size - 1 - row;
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (row == col)
+                    {
+                        primaryDiagonalSum += matrix[row, col];
+                    }
+                    if (col == neededIndex)
+                    {
+                        secondaryDiagonalSum += matrix[row,neededIndex];
+                    }
+                }
+            }
+            Console.WriteLine(Math.Abs(primaryDiagonalSum - secondaryDiagonalSum));
+        }
+    }
+}
