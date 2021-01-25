ef1a20c38279f7fc503bf8acdb01966a3e6b262c
Bogdan Angelov <bgd.angelov@gmail.com>
2021-01-25 13:12:48 +0200

    Add to Streams and Files

---
+++ 03.C# Advanced/04. Streams - Files And Directories/05. Even Lines/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _05._Even_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../text.txt"))
+            {
+                string line = reader.ReadLine();
+                Regex pattern = new Regex(@"[\!\,\.\?\-]");
+                List<string> reversedWords = new List<string>();
+                int counter = 0;
+
+                using (StreamWriter writer = new StreamWriter("../../../output.txt"))
+                {
+                    while (line != null)
+                    {
+                        string newLine = pattern.Replace(line, "@");
+                        reversedWords = newLine.Split(" ", StringSplitOptions.RemoveEmptyEntries).Reverse().ToList();
+                        StringBuilder finalLine = new StringBuilder();
+                        foreach (var word in reversedWords)
+                        {
+                            finalLine.Append(word + " ");
+                        }
+                        if (counter % 2 == 0)
+                        {
+                            writer.WriteLine(finalLine.ToString().TrimEnd());
+                        }
+                        line = reader.ReadLine();
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 03.C# Advanced/04. Streams - Files And Directories/06. LineNumbersTwo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace _06._LineNumbersTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] initialLines = File.ReadAllLines("../../../text.txt");
+            int letterCount = 0;
+            int punctCount = 0;
+
+            for (int i = 0; i < initialLines.Length; i++)
+            {
+                for (int j = 0; j < initialLines[i].Length; j++)
+                {
+                    if (!char.IsWhiteSpace(initialLines[i][j]) && !char.IsDigit(initialLines[i][j]))
+                    {
+                        if (char.IsLetter(initialLines[i][j]))
+                        {
+                            letterCount++;
+                        }
+                        else
+                        {
+                            punctCount++;
+                        }
+                    }
+                }
+
+                initialLines[i] = $"Line {i + 1}: {initialLines[i]} ({letterCount})({punctCount})";
+                letterCount = 0;
+                punctCount = 0;
+            }
+
+            File.WriteAllLines("../../../output.txt", initialLines);
+        }
+    }
+}
