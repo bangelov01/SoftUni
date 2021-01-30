ece8d2739b3694afdcdbf974923787145e4ef7ea
Bogdan Angelov <bgd.angelov@gmail.com>
2021-01-30 16:09:01 +0200

    Add to Functional Programming

---
+++ 03.C# Advanced/05. Functional Programming/15. PredicateParty/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _15._PredicateParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> peopleComing = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "Party!")
+            {
+                string[] commandArr = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string action = commandArr[0];
+                string rule = commandArr[1];
+                string toAdjust = commandArr[2];
+
+                Predicate<string> executeCommand = GetPredicate(rule, toAdjust);
+
+                if (action == "Remove")
+                {
+                    peopleComing.RemoveAll(executeCommand);
+                }
+                else if (action == "Double")
+                {
+                    List<string> holder = new List<string>();
+                    holder = peopleComing.FindAll(executeCommand);
+                    foreach (var perosn in holder)
+                    {
+                        int index = peopleComing.IndexOf(perosn);
+                        peopleComing.Insert(index, perosn);
+                    }
+                }
+            }
+            if (peopleComing.Count > 0)
+            {
+                Console.WriteLine($"{ string.Join(", ", peopleComing.ToList())} are going to the party!");
+            }
+            else
+            {
+                Console.WriteLine("Nobody is going to the party!");
+            }
+        }
+        public static Predicate<string> GetPredicate(string rule, string toAdjust)
+        {
+            if (rule == "StartsWith")
+            {
+                return n => n.StartsWith(toAdjust);
+            }
+            else if (rule == "EndsWith")
+            {
+                return n => n.EndsWith(toAdjust);
+            }
+            else
+            {
+                int length = int.Parse(toAdjust);
+                return n => n.Length == length;
+            }
+        }
+
+    }
+}
---
+++ 03.C# Advanced/05. Functional Programming/16. Reservation Module/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _16._Reservation_Module
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> invitedNames = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Dictionary<string, Dictionary<string, List<string>>> filteredHolder = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "Print")
+            {
+                string[] commandArr = command.Split(";", StringSplitOptions.RemoveEmptyEntries);
+                string action = commandArr[0];
+                string filterType = commandArr[1];
+                string parameter = commandArr[2];
+
+                Func<string, bool> predicate = GetPredicate(filterType, parameter);
+
+                if (action == "Add filter")
+                {
+                    List<string> holder = new List<string>();
+                    holder = invitedNames.Where(predicate).ToList();
+
+                    if (!filteredHolder.ContainsKey(filterType))
+                    {
+                        filteredHolder.Add(filterType, new Dictionary<string, List<string>>());
+                        filteredHolder[filterType].Add(parameter, holder);
+                    }
+                    else if (!filteredHolder[filterType].ContainsKey(parameter))
+                    {
+                        filteredHolder[filterType].Add(parameter, holder);
+                    }
+                    else
+                    {
+                        filteredHolder[filterType][parameter].AddRange(holder);
+                    }
+                }
+                else if (action == "Remove filter")
+                {
+                    filteredHolder[filterType].Remove(parameter);
+                }
+            }
+
+            foreach (var key in filteredHolder)
+            {
+                foreach (var param in filteredHolder[key.Key])
+                {
+                    foreach (var name in param.Value)
+                    {
+                        if (invitedNames.Contains(name))
+                        {
+                            invitedNames.Remove(name);
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", invitedNames));
+        }
+
+        static Func<string, bool> GetPredicate(string filterType, string parameter)
+        {
+            if (filterType == "Starts with")
+            {
+                return name => name.StartsWith(parameter);
+            }
+            else if (filterType == "Ends with")
+            {
+                return name => name.EndsWith(parameter);
+            }
+            else if (filterType == "Length")
+            {
+                int length = int.Parse(parameter);
+                return name => name.Length == length;
+            }
+            else
+            {
+                return name => name.Contains(parameter);
+            }
+        }
+    }
+}
