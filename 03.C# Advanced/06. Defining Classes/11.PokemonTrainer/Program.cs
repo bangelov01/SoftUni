062c8efd251193dafe05325b8b169ad8c0bc7340
Bogdan Angelov <bgd.angelov@gmail.com>
2021-02-05 13:51:11 +0200

    Add to Defining Classes

---
+++ 03.C# Advanced/06. Defining Classes/11.PokemonTrainer/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string command = string.Empty;
+            List<Trainer> trainers = new List<Trainer>();
+
+            while ((command = Console.ReadLine()) != "Tournament")
+            {
+                string[] commandArr = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string trainerName = commandArr[0];
+                string pokemonName = commandArr[1];
+                string pokemonElement = commandArr[2];
+                int pokemonHealth = int.Parse(commandArr[3]);
+
+                Pokemon newPokemon = new Pokemon(pokemonName,pokemonElement, pokemonHealth);
+
+                Trainer newTrainer = new Trainer(trainerName);
+
+                if (!trainers.Exists(x => x.Name == trainerName))
+                {
+                    newTrainer.Pokemon.Add(newPokemon);
+                    trainers.Add(newTrainer);
+                }
+                else
+                {
+                    Trainer addPoke = trainers.Find(x => x.Name == trainerName);
+                    addPoke.Pokemon.Add(newPokemon);
+                }
+            }
+
+            string commandPokemon = string.Empty;
+
+            while ((commandPokemon = Console.ReadLine()) != "End")
+            {
+                Func<Trainer, bool> isContained = CheckContainment(commandPokemon);
+
+                foreach (var trainer in trainers)
+                {
+                    if (isContained(trainer))
+                    {
+                        trainer.NumberOfBadges++;
+                    }
+                    else
+                    {
+                        trainer.Pokemon.ForEach(x => x.Health -= 10);
+                        trainer.Pokemon.RemoveAll(x => x.Health <= 0);
+                    }
+                }
+            }
+
+            foreach (var trainer in trainers.OrderByDescending(x => x.NumberOfBadges).Distinct())
+            {
+                Console.WriteLine($"{trainer.Name} {trainer.NumberOfBadges} {trainer.Pokemon.Count}");
+            }
+        }
+
+        public static Func<Trainer, bool> CheckContainment (string command)
+        {
+            return p => p.Pokemon.Any(x => x.Element == command);
+        }
+    }
+}
---
+++ 03.C# Advanced/06. Defining Classes/11.PokemonTrainer/Trainer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Trainer
+    {
+        private int numberOfBadges = 0;
+
+        public Trainer(string name)
+        {
+            Name = name;
+            NumberOfBadges = numberOfBadges;
+            Pokemon = new List<Pokemon>();
+        }
+
+        public Trainer(string name, int numberOfBadges) : this(name)
+        {
+            NumberOfBadges = numberOfBadges;
+        }
+
+        public string Name { get; set; }
+        public int NumberOfBadges { get; set; }
+        public List<Pokemon> Pokemon { get; set; }
+
+    }
+}
