4ed9b76d3d5d146d8cfe9eefc554e57322f6e12a
Bogdan Angelov <bgd.angelov@gmail.com>
2021-02-06 12:32:23 +0200

    Add CustomStackGeneric to Defining Classes

---
+++ 03.C# Advanced/06. Defining Classes/13.CustomStackGeneric/CustomStack.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _13.CustomStackGeneric
+{
+    public class CustomStack <T>
+    {
+        private T[] items;
+        private const int initialCapacity = 4;
+
+        public CustomStack()
+        {
+            items = new T[initialCapacity];
+        }
+        public int Count { get; private set; }
+
+        public void Push(T item)
+        {
+            if (items.Length == Count)
+            {
+                Resize();
+            }
+
+            items[Count] = item;
+            Count++;
+        }
+
+        public T Pop()
+        {
+            if (items.Length == 0)
+            {
+                throw new InvalidOperationException("Stack is empty!");
+            }
+
+            int lastIndex = Count - 1;
+            T last = items[lastIndex];
+            items[lastIndex] = default;
+            Count--;
+            return last;
+        }
+
+        public T Peek()
+        {
+            if (items.Length == 0)
+            {
+                throw new InvalidOperationException("Stack is empty!");
+            }
+
+            int lastIndex = Count - 1;
+            T last = items[lastIndex];
+            return last;
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            for (int i = Count - 1; i >= 0; i--)
+            {
+                action(items[i]);
+            }
+        }
+        private void Resize()
+        {
+            T[] copy = new T[initialCapacity * 2];
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                copy[i] = items[i];
+            }
+
+            items = copy;
+        }
+    }
+}
---
+++ 03.C# Advanced/06. Defining Classes/13.CustomStackGeneric/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _13.CustomStackGeneric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomStack<string> myStack = new CustomStack<string>();
+
+            string[] names = new string[5] { "Miko", "Pesho", "Stamat", "Gatio", "Petko" };
+
+            foreach (var name in names)
+            {
+                myStack.Push(name);
+            }
+
+            string nameToSearch = "Pesho";
+
+            for (int i = 0; i < myStack.Count; i++)
+            {
+                string item = myStack.Peek();
+
+                if (item != nameToSearch)
+                {
+                    myStack.Pop();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            myStack.ForEach(x =>
+            {
+                Console.WriteLine(x);
+            });
+        }
+    }
+}
