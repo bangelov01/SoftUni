9b8c84c477dc06a588b76b97c359a1f1ab7bfbc5
Bogdan Angelov <bgd.angelov@gmail.com>
2021-02-19 12:52:49 +0200

    Add problem to Functional programming

---
+++ 03.C# Advanced/05. Functional Programming/17.TriFunction/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _17.TriDunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            var names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            Func<string, int, bool> mainFunc = (a, b) => a.Sum(c => c) >= b;
+            Func<Func<string, int, bool>, List<string>, string> finalFunc = (a, b) => b.FirstOrDefault(c => a(c, number));
+
+            string result = finalFunc(mainFunc, names);
+
+            if (result != null)
+            {
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine(string.Empty);
+            }
+
+        }
+    }
+}
