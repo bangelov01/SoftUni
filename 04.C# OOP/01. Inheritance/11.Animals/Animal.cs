96a9f6ba423b45746d4ae00f80855b13eab6d30a
Bogdan Angelov <bgd.angelov@gmail.com>
2021-02-27 13:29:40 +0200

    Add to C# OOP problems

---
+++ 04.C# OOP/01. Inheritance/11.Animals/Animal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        protected Animal(string name, int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public string Gender { get; private set; }
+        public abstract string ProduceSound();
+
+        public override string ToString()
+        {
+            return $"{GetType().Name}" +
+                $"{Environment.NewLine}" +
+                $"{Name} {Age} {Gender}";
+        }
+    }
+}
---
+++ 04.C# OOP/01. Inheritance/11.Animals/StartUp.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "Beast!")
+
+             {
+                string[] info = Console.ReadLine().Split();
+                string name = info[0];
+                int age = int.Parse(info[1]);
+                string gender = info[2];
+
+                if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(gender) || age < 0)
+                {
+                    Console.WriteLine("Invalid input!");
+                    continue;
+                }
+
+                switch (command)
+                {
+                    case "Cat":
+                        Cat cat = new Cat(name, age,gender);
+                        animals.Add(cat);
+                        break;
+                    case "Dog":
+                        Dog dog = new Dog(name, age, gender);
+                        animals.Add(dog);
+                        break;
+                    case "Frog":
+                        Frog frog = new Frog(name, age, gender);
+                        animals.Add(frog);
+                        break;
+                    case "Kitten":
+                        Kitten kitten = new Kitten(name,age);
+                        animals.Add(kitten);
+                        break;
+                    case "Tomcat":
+                        Tomcat tomcat = new Tomcat(name, age);
+                        animals.Add(tomcat);
+                        break;
+                }
+
+            }
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal);
+                Console.WriteLine(animal.ProduceSound());
+            }
+        }
+    }
+}
