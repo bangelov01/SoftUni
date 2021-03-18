4ab4b26fcc2988586541e91d9a96471457243170
Bogdan Angelov <bgd.angelov@gmail.com>
2021-03-18 15:12:01 +0200

    Add to Reflection

---
+++ 04.C# OOP/06.Reflection/01.Stealer/Spy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string classToInvestigate, params string[] namesOfFields)
+        {
+            Type type = Type.GetType(classToInvestigate);
+            FieldInfo[] fields = type.GetFields
+                (BindingFlags.Public | 
+                BindingFlags.NonPublic |
+                BindingFlags.Instance |
+                BindingFlags.Static);
+
+            StringBuilder sb = new StringBuilder();
+
+            Object classInstance = Activator.CreateInstance(type, new object[] { });
+
+            sb.AppendLine($"Class under investigation: {classToInvestigate}");
+
+            foreach (FieldInfo field in fields.Where(f => namesOfFields.Contains(f.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ 04.C# OOP/06.Reflection/03.MissionPrivateImpossible/Spy.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string classToInvestigate, params string[] namesOfFields)
+        {
+            Type type = Type.GetType(classToInvestigate);
+            FieldInfo[] fields = type.GetFields
+                (BindingFlags.Public | 
+                BindingFlags.NonPublic |
+                BindingFlags.Instance |
+                BindingFlags.Static);
+
+            StringBuilder sb = new StringBuilder();
+
+            Object classInstance = Activator.CreateInstance(type, new object[] { });
+
+            sb.AppendLine($"Class under investigation: {classToInvestigate}");
+
+            foreach (FieldInfo field in fields.Where(f => namesOfFields.Contains(f.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        public string AnalyzeAccessModifiers(string classToInvestigate)
+        {
+            Type type = Type.GetType(classToInvestigate);
+
+            FieldInfo[] classFields = type.GetFields
+                (BindingFlags.Instance |
+                BindingFlags.Static |
+                BindingFlags.Public);
+            MethodInfo[] classPublicMethods = type.GetMethods
+                (BindingFlags.Instance |
+                BindingFlags.Public);
+            MethodInfo[] classNonPublic = type.GetMethods
+                (BindingFlags.Instance |
+                BindingFlags.NonPublic);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (FieldInfo field in classFields)
+            {
+                sb.AppendLine($"{field.Name} must be private!");
+            }
+            foreach (MethodInfo method in classPublicMethods.Where(m => m.Name.StartsWith("set")))
+            {
+                sb.AppendLine($"{method.Name} have to be private!");
+            }
+            foreach (MethodInfo method in classNonPublic.Where(m => m.Name.StartsWith("get")))
+            {
+                sb.AppendLine($"{method.Name} have to be public!");
+            }
+            return sb.ToString().Trim();
+        }
+
+        public string RevealPrivateMethods(string classToInvestigate)
+        {
+            Type type = Type.GetType(classToInvestigate);
+            MethodInfo[] classPrivateMethods = type.GetMethods
+                (BindingFlags.NonPublic |
+                BindingFlags.Instance);
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"All Private Methods of Class: {type.Name}");
+            sb.AppendLine($"Base Class: {type.BaseType.Name}");
+
+            foreach (MethodInfo method in classPrivateMethods)
+            {
+                sb.AppendLine(method.Name);
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
