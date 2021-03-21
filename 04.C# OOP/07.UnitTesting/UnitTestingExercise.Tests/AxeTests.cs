b5e07671b5eb440f5353002ac5bd4b5981c7ff8f
Bogdan Angelov <bgd.angelov@gmail.com>
2021-03-21 13:59:45 +0200

    Add UnitTestingExercise with base project and .Test

---
+++ 04.C# OOP/07.UnitTesting/UnitTestingExercise.Tests/AxeTests.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using System;
+using UnitTestingExercise;
+
+[TestFixture]
+public class AxeTests
+{
+    private int attack;
+    private int durability;
+    private int dummyHealth;
+    private int dummyExp;
+    private Axe axe;
+    private Dummy dummy;
+
+    [SetUp]
+    public void SetUp()
+    {
+        attack = 5;
+        durability = 10;
+        dummyHealth = 100;
+        dummyExp = 100;
+        axe = new Axe(5, 10);
+        dummy = new Dummy(dummyHealth, dummyExp);
+    }
+
+    [Test]
+    public void When_AxeIsInitialized_WithAttackPoints_AttackPointsShouldBeSet()
+    {
+        Assert.AreEqual(axe.AttackPoints, attack);
+    }
+
+    [Test]
+    public void When_AxeIsInitialized_WithDurabilityPoints_DurabilityPointssShouldBeSet()
+    {
+        Assert.AreEqual(axe.DurabilityPoints, durability);
+    }
+
+    [Test]
+    public void When_AxeAttacks_DurabilityPoints_ShouldBeLost()
+    {
+        axe.Attack(dummy);
+
+        Assert.AreEqual(axe.DurabilityPoints, --durability);
+    }
+
+    [Test]
+    public void When_AxeDurability_BecomesLessOrEqualToZero_ExceptionShouldBeThrown()
+    {
+        var ex = Assert.Throws<InvalidOperationException>(() =>
+        {
+            for (int i = 0; i <= durability; i++)
+            {
+                axe.Attack(dummy);
+            }
+        });
+
+        Assert.That(ex.Message, Is.EqualTo("Axe is broken."));
+    }
+}
---
+++ 04.C# OOP/07.UnitTesting/UnitTestingExercise.Tests/DummyTests.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using System;
+using UnitTestingExercise;
+
+[TestFixture]
+public class DummyTests
+{
+
+    private int dummyHealth;
+    private int dummyExperience;
+    private Dummy dummy;
+
+    [SetUp]
+    public void SetUp()
+    {
+        dummyHealth = 100;
+        dummyExperience = 100;
+        dummy = new Dummy(dummyHealth, dummyExperience);
+    }
+
+    [Test]
+    public void When_DummyIsInitiated_DummyHealth_ShouldBeSet()
+    {
+        Assert.AreEqual(dummy.Health, dummyHealth);
+    }
+
+    [Test]
+    public void When_Attacked_DummyShould_LoseHealth()
+    {
+        int attackPoints = 10;
+        dummy.TakeAttack(attackPoints);
+
+        Assert.AreEqual(dummy.Health, (dummyHealth - attackPoints));
+    }
+
+    [Test]
+    public void When_Dead_DummyShould_ThrowException()
+    {
+        int attackPoints = 110;
+
+        var ex = Assert.Throws<InvalidOperationException>(() =>
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                dummy.TakeAttack(attackPoints);
+            }
+        });
+    }
+
+    [Test]
+    public void When_Dead_DummyShould_GiveExperience()
+    {
+        dummy = new Dummy(0, dummyExperience);
+
+        Assert.AreEqual(dummy.GiveExperience(), dummyExperience);
+    }
+
+    [Test]
+    public void When_Alive_DummyShould_NotGiveExperience()
+    {
+        var ex = Assert.Throws<InvalidOperationException>(() => dummy.GiveExperience());
+
+        Assert.AreEqual(ex.Message, "Target is not dead.");
+    }
+}
+
