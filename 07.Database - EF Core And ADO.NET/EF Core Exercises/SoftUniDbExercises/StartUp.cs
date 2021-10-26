50dbdd33fc52313112dd23a23f7a17695436f3e9
Bogdan Angelov <bgd.angelov@gmail.com>
2021-10-26 15:42:50 +0300

    Add exercises for SoftUniDb - EF Core

---
+++ 07.Database - EF Core And ADO.NET/EF Core Exercises/SoftUniDbExercises/StartUp.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+            //var employees = GetEmployeesFullInformation(context);
+            //var employees = GetEmployeesWithSalaryOver50000(context);
+            //var employees = GetEmployeesFromResearchAndDevelopment(context);
+            var employees = AddNewAddressToEmployee(context);
+            Console.WriteLine(employees);
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .OrderBy(e => e.EmployeeId)
+                .Select(e => new
+                {
+                    FirstName = e.FirstName,
+                    MiddleName = e.MiddleName,
+                    LastName = e.LastName,
+                    JobTitle = e.JobTitle,
+                    Salary = $"{e.Salary:f2}"
+                }).ToList();
+
+            var sb = new StringBuilder();
+
+            foreach (var e in employees)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .OrderBy(e => e.FirstName)
+                .Where(e => e.Salary > 50000)
+                .Select(e => new
+                {
+                    Name = e.FirstName,
+                    Salary = e.Salary
+                }).ToArray();
+
+            var sb = new StringBuilder();
+
+            foreach (var e in employees)
+            {
+                sb.AppendLine($"{e.Name} - {e.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Where(e => e.Department.Name == "Research and Development")
+                .OrderBy(e => e.Salary)
+                .ThenByDescending(e => e.FirstName)
+                .Select(e => new
+                {
+                    FirstName = e.FirstName,
+                    LastName = e.LastName,
+                    DepartmenName = e.Department.Name,
+                    Salary = e.Salary
+                }).ToArray();
+
+            var sb = new StringBuilder();
+
+            foreach (var e in employees)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} from {e.DepartmenName} - ${e.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string AddNewAddressToEmployee(SoftUniContext context)
+        {
+
+            var neededEmployee = context.Employees
+                .FirstOrDefault(e => e.LastName == "Nakov");
+
+            neededEmployee.Address = new Address()
+            {
+                AddressText = "Vitoshka 15",
+                TownId = 4
+            };
+
+            context.SaveChanges();
+
+            var employeesAddress = context.Employees
+                .OrderByDescending(e => e.AddressId)
+                .Take(10)
+                .Select(ea => new
+                {
+                    EmployeeAddress = ea.Address.AddressText
+                }).ToArray();
+
+            var sb = new StringBuilder();
+
+            foreach (var a in employeesAddress)
+            {
+                sb.AppendLine(a.EmployeeAddress);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
