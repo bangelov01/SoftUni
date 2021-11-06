3c7318936e98df98b7ba07ef14d41f8e145349af
Bogdan Angelov <bgd.angelov@gmail.com>
2021-11-06 15:03:43 +0200

    More query exercises

---
+++ 07.Database - EF Core And ADO.NET/EF Core Exercises/CodeFirstExercises/BookShop/BookShop/StartUp.cs
@@ -0,0 +1,160 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            //string command = Console.ReadLine().ToLower();
+            //string result = GetBooksByAgeRestriction(db, command);
+
+            //string result = GetGoldenBooks(db);
+            //string result = GetBooksByPrice(db);
+
+            //int year = int.Parse(Console.ReadLine());
+            //string result = GetBooksNotReleasedIn(db, year);
+
+            //string input = Console.ReadLine();
+            //var result = GetBooksByCategory(db, input);
+
+            //string date = Console.ReadLine();
+
+            //var result = GetBooksReleasedBefore(db, date);
+
+            string input = Console.ReadLine();
+
+            var result = GetAuthorNamesEndingIn(db, input);
+
+            Console.WriteLine(result);
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var convert = Enum.Parse<AgeRestriction>(command, true);
+
+            var books = context.Books
+                .Where(b => b.AgeRestriction == convert)
+                .Select(b => b.Title)
+                .OrderBy(t => t)
+                .ToArray();
+
+            var result = string.Join(Environment.NewLine, books);
+
+            return result;
+        }
+
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            var neededEnum = Enum.Parse<EditionType>("Gold", true);
+
+            var books = context.Books
+                .Where(b => b.EditionType == neededEnum &&
+                            b.Copies < 5000)
+                .OrderBy(b => b.BookId)
+                .Select(b => b.Title)
+                .ToArray();
+
+            var result = string.Join(Environment.NewLine, books);
+
+            return result;
+
+        }
+
+        public static string GetBooksByPrice(BookShopContext context)
+        {
+            var books = context.Books
+                .Select(b => new
+                {
+                    b.Title,
+                    b.Price
+                })
+                .Where(b => b.Price > 40)
+                .OrderByDescending(b => b.Price)
+                .ToArray();
+
+            var sb = new StringBuilder();
+
+            foreach (var b in books)
+            {
+                sb.AppendLine($"{b.Title} - ${b.Price:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetBooksNotReleasedIn(BookShopContext context, int year)
+        {
+            var books = context.Books
+                .Where(b => b.ReleaseDate.Value.Year != year)
+                .OrderBy(b => b.BookId)
+                .Select(b => b.Title)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, books);
+        }
+
+        public static string GetBooksByCategory(BookShopContext context, string input)
+        {
+            string[] arr = input.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.ToLower())
+                .ToArray();
+
+            var categories = context.BooksCategories
+                .Where(bc => arr.Contains(bc.Category.Name.ToLower()))
+                .Select(b => b.Book.Title)
+                .OrderBy(t => t)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, categories);
+        }
+
+        public static string GetBooksReleasedBefore(BookShopContext context, string date)
+        {
+            var parsedDate = DateTime.ParseExact(date, "dd-MM-yyyy", CultureInfo.InvariantCulture);
+
+            var books = context.Books
+                .Where(b => b.ReleaseDate < parsedDate)
+                .OrderByDescending(b => b.ReleaseDate)
+                .Select(b => new
+                {
+                    b.Title,
+                    Edition = b.EditionType.ToString(),
+                    Price = b.Price.ToString("f2")
+                })
+                .ToArray();
+
+            var sb = new StringBuilder();
+
+            foreach (var b in books)
+            {
+                sb.AppendLine($"{b.Title} - {b.Edition} - ${b.Price}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetAuthorNamesEndingIn(BookShopContext context, string input)
+        {
+            var authors = context.Authors
+                .ToArray()
+                .Where(a => EF.Functions.Like(a.FirstName, $"%{input}"))
+                .Select(a => $"{a.FirstName} {a.LastName}")
+                .OrderBy(a => a)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, authors);
+        }
+    }
+}
