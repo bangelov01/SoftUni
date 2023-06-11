fd4fab7b1621cdef0e11e65b934f3ccec0034e3d
bangelov01 <bgd.angelov@gmail.com>
2023-06-11 14:14:47 +0300

    config azure taskboard app with sqlserver db

---
+++ 11.Containerization and Cloud/Terraform/azure_taskboard_app_with_db/azure_taskboard_app_with_db_config.tf
@@ -0,0 +1,91 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Generate a random integer to create a globally unique name
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "TaskBoardRG${random_integer.ri.result}"
+  location = "West Europe"
+}
+
+# Create a Linux app service plamn
+resource "azurerm_service_plan" "cbsp" {
+  name                = "task-board-plan-${random_integer.ri.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+# Create a web app with the service plan ID
+resource "azurerm_linux_web_app" "cbapp" {
+  name                = "task-board-${random_integer.ri.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.cbsp.id
+
+  site_config {
+    application_stack {
+      dotnet_version = "6.0"
+    }
+    always_on = false
+  }
+
+  connection_string {
+    name  = "DefaultConnection"
+    type  = "SQLAzure"
+    value = "Data Source=tcp:${azurerm_mssql_server.sqlserver.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.sqldb.name};User ID=${azurerm_mssql_server.sqlserver.administrator_login};Password=${azurerm_mssql_server.sqlserver.administrator_login_password};Trusted_Connection=False;MultipleActiveResultSets=True;"
+  }
+}
+
+# Deploy code from a public Git repo
+resource "azurerm_app_service_source_control" "sc" {
+  app_id                 = azurerm_linux_web_app.cbapp.id
+  repo_url               = "https://github.com/bangelov01/TaskBoard"
+  branch                 = "main"
+  use_manual_integration = true
+}
+
+# Create an Sql Server instance
+resource "azurerm_mssql_server" "sqlserver" {
+  name                         = "task-board-sql-${random_integer.ri.result}"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+}
+
+# Create database
+resource "azurerm_mssql_database" "sqldb" {
+  name           = "taskboardDB${random_integer.ri.result}"
+  server_id      = azurerm_mssql_server.sqlserver.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  sku_name       = "S0"
+  zone_redundant = false
+}
+
+# Create mssql firewall rule
+resource "azurerm_mssql_firewall_rule" "fw" {
+  name             = "taskboardFW${random_integer.ri.result}"
+  server_id        = azurerm_mssql_server.sqlserver.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
